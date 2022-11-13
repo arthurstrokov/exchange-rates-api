d268ef7e03de21636298ead80c268d288cd1ae9f
Arthur <arthurstrokov@gmail.com>
2022-11-13 17:15:12 +0300

    up exception logic, add ObjectMapper

--- src/main/java/com/gmail/arthurstrokov/exchangeratesapi/exceptions/RetreiveMessageErrorDecoder.java
+++ src/main/java/com/gmail/arthurstrokov/exchangeratesapi/exceptions/RetreiveMessageErrorDecoder.java
@@ -1,11 +1,11 @@
 package com.gmail.arthurstrokov.exchangeratesapi.exceptions;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.gmail.arthurstrokov.exchangeratesapi.configuration.ExceptionMessage;
 import feign.Response;
 import feign.RetryableException;
 import feign.codec.ErrorDecoder;
-
-import java.time.LocalDateTime;
+import lombok.SneakyThrows;
 
 /**
  * @author Артур Александрович Строков
@@ -15,17 +15,21 @@ import java.time.LocalDateTime;
 public class RetreiveMessageErrorDecoder implements ErrorDecoder {
     private final ErrorDecoder errorDecoder = new Default();
 
+    @SneakyThrows
     @Override
     public Exception decode(String methodKey, Response response) {
         ExceptionMessage message = new ExceptionMessage();
-        message.setTimestamp(String.valueOf(LocalDateTime.now()));
         message.setStatus(response.status());
         message.setReason(response.reason());
 
+        ObjectMapper objectMapper = new ObjectMapper();
+        String errorMessage = objectMapper.writeValueAsString(message);
+
         return switch (response.status()) {
-            case 400 -> new BadRequestException(message.getReason() != null ? message.getReason() : "Bad Request");
-            case 404 -> new NotFoundException(message.getReason() != null ? message.getReason() : "Not found");
-            case 500 -> new RetryableException(500, response.reason(), response.request().httpMethod(), null, response.request());
+            case 400 -> new BadRequestException(errorMessage);
+            case 404 -> new NotFoundException(errorMessage);
+            case 500 ->
+                    new RetryableException(500, response.reason(), response.request().httpMethod(), null, response.request());
             default -> errorDecoder.decode(methodKey, response);
         };
     }
